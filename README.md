cf548c1b466cb696e7b2f28eb9fe13027d71e1dc
Jillian Gartner <jilliangartner@Jillians-MacBook-Pro.local>
2022-05-21 18:19:39 -0700

    readme generator

--- README.md
+++ README.md
@@ -1,1 +1,63 @@
-# README-Generator
+# README Generator
+
+![badge](https://img.shields.io/github/languages/top/jrgartner/README-Generator)
+<br>
+![badge](https://img.shields.io/github/languages/count/jrgartner/README-Generator)
+<br>
+![badge](https://img.shields.io/github/issues/jrgartner/README-Generator)
+<br>
+![badge](https://img.shields.io/github/issues-closed/jrgartner/README-Generator)
+<br>
+![badge](https://img.shields.io/github/last-commit/jrgartner/README-Generator)
+<br>
+![badge](https://img.shields.io/badge/license-AAL-important)
+
+## Description
+
+create a readme generator using javascript, node and npm
+
+## Table of Contents
+
+- [Description](#description)
+- [Documentation](#documentation)
+- [Usage](#usage)
+- [Screenshot](#screenshot)
+- [Features](#features)
+- [Acknowledgements](#acknowledgements)
+- [License](#license)
+- [Testing](#testing)
+- [Contact](#contact)
+
+## Documentation
+
+download the zip files, open the files in vs code and run using node
+
+## Usage
+
+up to the user
+
+## Screenshot
+
+![Screenshot](./assets/Screen Shot 2022-05-21 at 6.17.12 PM.png)
+
+## Features
+
+node js
+
+# Acknowledgements
+
+Jillian Gartner
+
+## License
+
+![badge](https://img.shields.io/badge/license-AAL-important)
+<br>
+Permission to use this application is granted under the AAL license. <https://opensource.org/licenses/AAL>
+
+## Testing
+
+N/A
+
+## Contact:
+
+Holler at me! <a href="mailto:jillianrosegartner@gmail.com">jillianrosegartner@gmail.com</a>
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,67 @@
+//    _____
+//   |A .  |
+//   | / \ |
+//   |( . )|
+//   |  T  |
+//   ---"---
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  //   return `# ${data.title}
+  return `# ${data.title}
+
+  ![badge](https://img.shields.io/github/languages/top/${data.github}/${data.repo})
+  <br> 
+  ![badge](https://img.shields.io/github/languages/count/${data.github}/${data.repo})
+  <br>
+  ![badge](https://img.shields.io/github/issues/${data.github}/${data.repo})
+  <br>
+  ![badge](https://img.shields.io/github/issues-closed/${data.github}/${data.repo})
+  <br>
+  ![badge](https://img.shields.io/github/last-commit/${data.github}/${data.repo})
+  <br>
+  ![badge](https://img.shields.io/badge/license-${data.license}-important)
+  
+  ## Description
+  
+   ${data.description}
+ 
+  ## Table of Contents
+  - [Description](#description)
+  - [Documentation](#documentation)
+  - [Usage](#usage)
+  - [Screenshot](#screenshot)
+  - [Features](#features)
+  - [Acknowledgements](#acknowledgements)
+  - [License](#license)
+  - [Testing](#testing)
+  - [Contact](#contact)
+
+  ## Documentation
+  ${data.documentation}
+ 
+  ## Usage
+  ${data.usage}
+
+  ## Screenshot
+  ![Screenshot](${data.screenshot})
+
+  ## Features
+  ${data.features}
+  
+  # Acknowledgements
+  ${data.acknowledgements}
+    
+  ## License
+  ![badge](https://img.shields.io/badge/license-${data.license}-important)
+  <br>
+  Permission to use this application is granted under the ${data.license} license. <https://opensource.org/licenses/${data.license}>
+
+
+  ## Testing
+  ${data.testing}
+
+  ## Contact:
+  Holler at me! <a href="mailto:${data.email}">${data.email}</a>`;
+}
+
+module.exports = generateMarkdown;
